d0857d4700acfa77347ae7309c158e3e003d59f8
Malyshev Valery <malyshevalery@gmail.com>
2019-12-02 13:15:58 +0300

    Bug with wrong batch length

--- imports/data/mask_generator.py
+++ imports/data/mask_generator.py
@@ -21,7 +21,7 @@ class MaskGenerator(Sequence):
 
     def __len__(self):
         """Denotes the number of batches per epoch"""
-        return int(np.floor(len(self.__idxs) / self.__batch_size))
+        return int(np.ceil(len(self.__idxs) / self.__batch_size))
 
     def __getitem__(self, index):
         """Generate one batch of data"""
