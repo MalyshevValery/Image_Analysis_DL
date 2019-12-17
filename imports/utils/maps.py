127cc36ea281f3b7033d5bc752cb977bb50812f6
Malyshev Valery <malyshevalery@gmail.com>
2019-12-17 10:26:43 +0300

    Remove segmentation models because of libC incompatibility on computational server

--- imports/utils/maps.py
+++ imports/utils/maps.py
@@ -1,23 +1,13 @@
 """Maps settings strings to functions and objects"""
 import albumentations as aug
-import segmentation_models.losses as losses
-import segmentation_models.metrics as metrics
 
 import imports.data.decorators as decorators
 import imports.data.loaders as loaders
 
 metrics_map = {
-    'iou': metrics.IOUScore(name='iou'),
-    'f1': metrics.FScore(beta=1, name='f1'),
-    'f2': metrics.FScore(beta=2, name='f2'),
-    'precision': metrics.Precision(name='precision'),
-    'recall': metrics.Recall(name='recall')
 }
 
 loss_map = {
-    'jaccard': losses.JaccardLoss(),
-    'dice': losses.DiceLoss(),
-    'binary_focal': losses.BinaryFocalLoss()
 }
 
 augmentations = {
