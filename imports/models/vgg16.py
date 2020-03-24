cc29df5f4695a739633d5af1b8d91be96d25cef7
Malyshev Valery <malyshevalery@gmail.com>
2020-03-24 13:24:30 +0300

    Average pooling

--- imports/models/vgg16.py
+++ imports/models/vgg16.py
@@ -2,13 +2,15 @@ from typing import Optional, Sequence
 
 from tensorflow.keras import Model
 from tensorflow.keras.applications import VGG16
-from tensorflow.keras.layers import Dense, Flatten, Input
+from tensorflow.keras.layers import Dense, Flatten, Input, \
+    GlobalAveragePooling2D
 
 
 def customVGG16(top: Sequence[int] = None, weights: Optional[str] = 'imagenet',
                 input_shape: Sequence[int] = None, pooling: str = None,
                 classes: int = 1000,
-                classifier_activation: str = 'softmax') -> Model:
+                classifier_activation: str = 'softmax',
+                ave: bool = False) -> Model:
     """Returns VGG16 with custom layers
 
     :param top: None to include VGG16 or sequence of FC channels
@@ -26,7 +28,10 @@ def customVGG16(top: Sequence[int] = None, weights: Optional[str] = 'imagenet',
     else:
         input_ = Input(shape=input_shape, name='image_input')
         output = vgg16(input_)
-        fc = Flatten()(output)
+        if ave:
+            fc = GlobalAveragePooling2D()(output)
+        else:
+            fc = Flatten()(output)
         for channels in top:
             fc = Dense(channels, activation='relu')(fc)
         custom_output = Dense(classes, activation=classifier_activation)(fc)
