3db0576bee56df3231df8ef1d1a8483683c41386
Malyshev Valery <malyshevalery@gmail.com>
2021-01-22 11:33:14 +0300

    Fixed prec rec

--- imagedl/nn/metrics/recall.py
+++ imagedl/nn/metrics/recall.py
@@ -10,28 +10,38 @@ from .metric import UpgradedMetric, sum_class_agg
 class Recall(UpgradedMetric):
     """Recall metric. Input: logits, targets"""
     def __init__(self, n_classes: int = 1,
-                 output_transform: Transform = lambda x: x):
+                 output_transform: Transform = lambda x: x, multi_label=False):
         super().__init__(output_transform=output_transform)
-        self._scores = torch.zeros(n_classes)
+        self._tp = torch.zeros(n_classes)
+        self._t = torch.zeros(n_classes)
         self._n_classes = n_classes
+        self._multi_label = multi_label
 
     def _reset(self) -> None:
         """Resets the metric"""
-        self._scores = torch.zeros(self._n_classes)
+        self._tp = torch.zeros(self._n_classes)
+        self._t = torch.zeros(self._n_classes)
 
     def _update(self, output: Tuple[torch.Tensor, torch.Tensor]) -> None:
         """Updates the metric"""
         logits, targets = output
-        self._scores = self._scores.to(logits.device)
+        self._tp = self._tp.to(logits.device)
+        self._t = self._t.to(logits.device)
         device = logits.device
 
-        pred = logits.argmax(1)
-        tp: torch.Tensor = pred == targets
-        values = torch.ones(int(tp.sum()), device=device)
-        tp = sum_class_agg(targets[tp], values, self._n_classes)
-        uq, cnt = targets.unique(return_counts=True)
-        self._scores[uq] += tp[uq] / cnt
+        if not self._multi_label:
+            pred = logits.argmax(1)
+            tp: torch.Tensor = pred == targets
+            values = torch.ones(int(tp.sum()), device=device)
+            self._tp += sum_class_agg(targets[tp], values, self._n_classes)
+            uq, cnt = targets.unique(return_counts=True)
+            self._t[uq] += cnt
+        else:
+            pred = logits > 0
+            tp = (pred * targets).sum(0)
+            self._tp += tp
+            self._t += targets.sum(0)
 
     def compute(self) -> torch.Tensor:
         """Metric aggregation"""
-        return self._scores / (self._updates + 1e-7)
+        return self._tp / (self._t + 1e-7)
