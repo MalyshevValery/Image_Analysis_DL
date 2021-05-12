a2a735a29791f7d655756a372bf4b3cb7e1ddca2
Malyshev Valery <malyshevalery@gmail.com>
2021-05-12 11:45:00 +0300

    Metric handling no warnings

--- imagedl/train_utils/metric_handling.py
+++ imagedl/train_utils/metric_handling.py
@@ -1,3 +1,4 @@
+"""Metric values and labels preprocessing"""
 from typing import Dict, List, Union, Mapping
 
 import torch
@@ -11,20 +12,24 @@ from imagedl.utils.plot_saver import PlotSave
 def clean_metrics(metrics: Mapping[str, Metric],
                   metric_values: Dict[str, Union[float, torch.Tensor]],
                   legend: List[str]) -> Dict[str, float]:
-    sorted_names = sorted(metric_values.keys())
+    """Clean metrics from 2+D metrics and
+    prettify output of tensor-like metrics"""
+    # TODO: Give legend a use
+    sorted_names = sorted(metrics.keys())
     res = {}
     for s in sorted_names:
-        if s != 'loss' and isinstance(metrics[s], UpgradedMetric):
-            if metrics[s].vis:
-                continue
-        if isinstance(metric_values[s], torch.Tensor):
-            if metric_values[s].shape == ():
-                res[s] = metric_values[s].item()
-            elif len(metric_values[s].shape) == 1:
-                for i in range(len(metric_values[s])):
-                    res[f'{s}_{i}'] = metric_values[s][i].item()
+        val = metric_values[s]
+        if isinstance(metrics[s], UpgradedMetric) and metrics[s].vis:
+            continue
+        if isinstance(val, torch.Tensor):
+            if val.shape == ():
+                res[s] = val.item()
+            elif len(val.shape) == 1:
+                for i in range(len(val)):
+                    res[f'{s}_{legend[i]}'] = val[i].item()
         else:
-            res[s] = metric_values[s]
+            res[s] = val
+    res['loss'] = float(metric_values['loss'])
     return res
 
 
@@ -41,11 +46,8 @@ def metrics_to_str(metrics: Mapping[str, Metric],
             with PlotSave(prefix + s, tb_logger, epoch):
                 metrics[s].visualize(metric_values[s], legend)
 
-    metric_values = clean_metrics(metrics, metric_values, legend)
+    clean_values = clean_metrics(metrics, metric_values, legend)
     res = []
-    for s in sorted(metric_values.keys()):
-        try:
-            res.append(f'{s}: {metric_values[s]:.3f}')
-        except TypeError:
-            pass
+    for s in sorted(clean_values.keys()):
+        res.append(f'{s}: {clean_values[s]:.3f}')
     return ' '.join(res)
