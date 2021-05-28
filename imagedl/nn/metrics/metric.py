Precision tests and fixes

imagedl/nn/metrics/metric.py
@@ -6,7 +6,7 @@ import torch
 from ignite.metrics import Metric
 from torch import Tensor
 
-from imagedl.data.datasets.abstract import Transform
+from imagedl.utils.types import MetricTransform
 
 
 class MeanMetric(Metric):
@@ -38,7 +38,8 @@ class UpgradedMetric(Metric):
     - visualize - visualize your metric in graph if possible (default - pass)
     """
 
-    def __init__(self, output_transform: Transform = lambda x: x,
+    def __init__(self,
+                 output_transform: MetricTransform = lambda x: x,
                  vis: bool = False) -> None:
         super().__init__(output_transform)
         self._apply_reset = True
@@ -61,7 +62,7 @@ class UpgradedMetric(Metric):
         raise NotImplementedError()
 
     def update(self, output: Tuple[torch.Tensor, torch.Tensor]) -> None:
-        """Update metric's internal state"""
+        """Update metric internal state"""
         if self._apply_reset:
             self._reset()
             self._apply_reset = False
@@ -94,5 +95,5 @@ def sum_class_agg(labels: Tensor, values: Tensor,
     :return:
     """
     res = torch.zeros(n_classes, device=values.device)
-    res.scatter_add_(0, labels, values)
+    res.scatter_add_(0, labels.long(), values)
     return res

imagedl/nn/metrics/precision.py
@@ -3,15 +3,26 @@ from typing import Tuple
 
 import torch
 
-from imagedl.data.datasets.abstract import Transform
 from imagedl.nn.metrics.metric import UpgradedMetric, sum_class_agg
+from imagedl.utils.types import MetricTransform
 
 
 class Precision(UpgradedMetric):
-    """Precision metric. Input: logits, targets"""
+    """Precision metric. Input: logits, targets
+    Shapes for binary:
+    - Logits [BatchSize]
+    - Targets {0,1} [BatchSize]
+    Shapes for multi-label:
+    - Logits [BatchSize, NClass]
+    - Targets {0,1} [BatchSize, NClass]
+    Shapes for classification:
+    - Logits [BatchSize, NClass]
+    - Targets {0,NClass-1} [BatchSize]
+    """
 
     def __init__(self, n_classes: int = 1,
-                 output_transform: Transform = lambda x: x, multi_label=False):
+                 output_transform: MetricTransform = lambda x: x,
+                 multi_label: bool = False):
         super().__init__(output_transform=output_transform)
         self._tp = torch.zeros(n_classes)
         self._p = torch.zeros(n_classes)
@@ -30,22 +41,15 @@ class Precision(UpgradedMetric):
         self._p = self._p.to(logits.device)
         device = logits.device
 
-        if not self._multi_label:
-            if self._n_classes > 1:
-                pred = logits.argmax(1)
-                tp: torch.Tensor = pred == targets
-                values = torch.ones(int(tp.sum()), device=device)
-                self._tp += sum_class_agg(targets[tp], values, self._n_classes)
-                uq, cnt = pred.unique(return_counts=True)
-                self._p[uq] += cnt
-            else:
-                pred = 1 * (logits > 0)
-                tp: torch.Tensor = pred * targets
-                self._tp[0] += tp.sum()
-                self._p[0] += pred.sum()
-
+        if not self._multi_label and self._n_classes > 1:
+            pred: torch.Tensor = logits.argmax(1)
+            tp: torch.Tensor = torch.eq(pred, targets)
+            values = torch.ones(int(tp.sum()), device=device)
+            self._tp += sum_class_agg(targets[tp], values, self._n_classes)
+            uq, cnt = pred.unique(return_counts=True)
+            self._p[uq] += cnt
         else:
-            pred = logits > 0
+            pred = torch.gt(logits, 0)
             tp = (pred * targets).sum(0)
             self._tp += tp
             self._p += pred.sum(0)

tests/nn/metrics/precision.py
@@ -7,17 +7,13 @@ from imagedl.nn.metrics import Precision
 
 
 class PrecisionTestCase(unittest.TestCase):
-    def test_binary(self):
+    def test_binary(self) -> None:
         self.__compare(Precision(),
-                       torch.Tensor([
-                           [1, 1, 1, 1, -1, -1]
-                       ]),
-                       torch.Tensor([
-                           [1, 1, 0, 0, 0, 1]
-                       ]),
+                       torch.Tensor([1, 1, 1, 1, -1, -1]),
+                       torch.Tensor([1, 1, 0, 0, 0, 1]),
                        torch.tensor([0.5]))
 
-    def test_multi_class(self):
+    def test_multi_class(self) -> None:
         self.__compare(Precision(3, multi_label=True),
                        torch.Tensor([
                            [1, 1, 1], [1, 1, 1],
@@ -31,7 +27,7 @@ class PrecisionTestCase(unittest.TestCase):
                        ]),
                        torch.Tensor([0.75, 1.0, 0.8]))
 
-    def test_class(self):
+    def test_class(self) -> None:
         self.__compare(Precision(3),
                        torch.Tensor([[1, 0, 0], [1, 0, 0],
                                      [0, 1, 0], [0, 1, 0],
@@ -39,25 +35,25 @@ class PrecisionTestCase(unittest.TestCase):
                        torch.Tensor([0, 0, 0, 1, 0, 0]),
                        torch.Tensor([1.0, 0.5, 0.0]))
 
-    def test_zeros(self):
+    def test_zeros(self) -> None:
         prec = Precision()
         zer = torch.zeros(1)
-        self.__compare(prec, torch.Tensor([[1, 1, 1, -1, -1, -1]]),
-                       torch.Tensor([[0, 0, 0, 1, 1, 1]]), zer)
-        self.__compare(prec, torch.Tensor([[-1, -1, -1, -1, -1, -1]]),
-                       torch.Tensor([[0, 0, 0, 1, 1, 1]]), zer)
+        self.__compare(prec, torch.Tensor([1, 1, 1, -1, -1, -1]),
+                       torch.Tensor([0, 0, 0, 1, 1, 1]), zer)
+        self.__compare(prec, torch.Tensor([-1, -1, -1, -1, -1, -1]),
+                       torch.Tensor([0, 0, 0, 1, 1, 1]), zer)
 
-    def test_two_updates(self):
+    def test_two_updates(self) -> None:
         prec = Precision()
-        self.__compare(prec, torch.Tensor([[-1, 1, 1, 1]]),
-                       torch.Tensor([[0, 0, 1, 1]]),
+        self.__compare(prec, torch.Tensor([-1, 1, 1, 1]),
+                       torch.Tensor([0, 0, 1, 1]),
                        torch.Tensor([0.6667]), False)
-        self.__compare(prec, torch.Tensor([[-1, -1, 1, 1]]),
-                       torch.Tensor([[0, 0, 0, 1]]),
+        self.__compare(prec, torch.Tensor([-1, -1, 1, 1]),
+                       torch.Tensor([0, 0, 0, 1]),
                        torch.Tensor([0.6]), False)
 
     def __compare(self, prec: Precision, inp: torch.Tensor, out: torch.Tensor,
-                  res: torch.Tensor, reset=True):
+                  res: torch.Tensor, reset: bool = True) -> None:
         prec.update((inp, out))
         compare = torch.isclose(prec.compute(), res, 1e-04)
         self.assertTrue(torch.all(compare).item())