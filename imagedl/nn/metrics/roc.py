ea2f7f89bd9796328a4c032f951902adf3c5bbb7
Malyshev Valery <malyshevalery@gmail.com>
2021-06-03 15:18:01 +0300

    ROC update

--- imagedl/nn/metrics/roc.py
+++ imagedl/nn/metrics/roc.py
@@ -4,8 +4,18 @@ from typing import Tuple, List
 import matplotlib.pyplot as plt
 import torch
 
-from imagedl.data.datasets.abstract import Transform
+from imagedl.utils.types import MetricTransform
 from .metric import UpgradedMetric
+
+
+def _calc_class(tar: torch.Tensor,
+                val: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    _sorted = torch.zeros(val.shape[0] + 1, device=tar.device)
+    _sorted[1:] = tar[torch.argsort(-val)]
+    tpr = torch.cumsum(_sorted, 0) / (tar.sum() + 1e-7)
+    _sorted[1:] = 1 - _sorted[1:]
+    fpr = torch.cumsum(_sorted, 0) / (len(tar) - tar.sum() + 1e-7)
+    return tpr, fpr
 
 
 class ROCCurve(UpgradedMetric):
@@ -18,13 +28,13 @@ class ROCCurve(UpgradedMetric):
     """
 
     def __init__(self, n_classes: int = 1,
-                 output_transform: Transform = lambda x: x,
-                 multilabel: bool = False):
+                 output_transform: MetricTransform = lambda x: x,
+                 multi_label: bool = False):
         super().__init__(output_transform, True)
         self._n_classes = n_classes
-        self._multilabel = multilabel
-        if n_classes == 1 and self._multilabel:
-            raise ValueError('For multilabel number of classes must exceed 1')
+        self._multi_label = multi_label
+        if n_classes == 1 and self._multi_label:
+            raise ValueError('For multi-label number of classes must exceed 1')
         self._values: List[torch.Tensor] = []
         self._targets: List[torch.Tensor] = []
 
@@ -35,7 +45,7 @@ class ROCCurve(UpgradedMetric):
     def _update(self, output: Tuple[torch.Tensor, torch.Tensor]) -> None:
         """Updates the metric"""
         logits, targets = output
-        if self._n_classes == 1 or self._multilabel:
+        if self._n_classes == 1 or self._multi_label:
             probs = torch.sigmoid(logits.detach())
         else:
             probs = torch.softmax(logits.detach(), 1)
@@ -47,36 +57,27 @@ class ROCCurve(UpgradedMetric):
         assert self._updates > 0
         values = torch.cat(self._values, 0)
         targets = torch.cat(self._targets, 0)
-        tprs = []
-        fprs = []
         if self._n_classes == 1:
-            tpr, fpr = self.__calc_class(targets, values)
+            tpr, fpr = _calc_class(targets, values)
             tprs = tpr[None]
             fprs = fpr[None]
         else:
+            tpr_list = []
+            fpr_list = []
             for cls in range(0, self._n_classes):
-                if self._multilabel:
+                if self._multi_label:
                     tar: torch.Tensor = targets[:, cls]
                 else:
-                    tar: torch.Tensor = 1.0 * (targets == cls)
+                    tar = 1.0 * torch.eq(targets, cls)
                 if len(values.shape) == 1:
                     dat = values
                 else:
                     dat = values[:, cls]
-                tpr, fpr = self.__calc_class(tar, dat)
-                tprs.append(tpr)
-                fprs.append(fpr)
-            tprs, fprs = torch.stack(tprs), torch.stack(fprs)
+                tpr, fpr = _calc_class(tar, dat)
+                tpr_list.append(tpr)
+                fpr_list.append(fpr)
+            tprs, fprs = torch.stack(tpr_list), torch.stack(fpr_list)
         return torch.stack([tprs, fprs])
-
-    def __calc_class(self, tar: torch.Tensor,
-                     val: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        _sorted = torch.zeros(val.shape[0] + 1, device=tar.device)
-        _sorted[1:] = tar[torch.argsort(-val)]
-        tpr = torch.cumsum(_sorted, 0) / (tar.sum() + 1e-7)
-        _sorted[1:] = 1 - _sorted[1:]
-        fpr = torch.cumsum(_sorted, 0) / (len(tar) - tar.sum() + 1e-7)
-        return tpr, fpr
 
     def visualize(self, value: torch.Tensor, legend: List[str] = None) -> None:
         """Visualizing of ROC curve
